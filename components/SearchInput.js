c84dc4bf90a127ef1bd65e2198273d34a1596f34
Herbert Izidro <herbertsouza80@yahoo.com>
2022-08-16 20:56:22 -0300

    checagem de props adc

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styles from '../styles/SearchInput.module.css'
 
 export default function SearchInput(props) {
-  const {search, onChangeFunc, onClickFunc} = props
+  const {search, onChangeFunc, onClickFunc, placeholder, buttonText} = props
   return (
 	<div id="input-search">
-		<input type="text" className="form-control rounded-0" placeholder="Search for a character" value={search} onChange={onChangeFunc}/>
-		<button type="button" style={{backgroundColor: "#70aa46", borderColor: "#70aa46"}} className="btn btn-success rounded-0" onClick={onClickFunc}>Search</button>
+		<input type="text" className="form-control rounded-0" placeholder={placeholder ? placeholder : ''} value={search} onChange={onChangeFunc}/>
+		<button type="button" style={{backgroundColor: "#70aa46", borderColor: "#70aa46"}} className="btn btn-success rounded-0" onClick={onClickFunc}>{buttonText}</button>
 	</div>
   )
 }
@@ -14,6 +14,8 @@ export default function SearchInput(props) {
 /* checagem de props */
 SearchInput.propTypes = {
   search: PropTypes.string,
+  placeholder: PropTypes.string,
+  buttonText: PropTypes.string.isRequired,	
   onChangeFunc: PropTypes.func.isRequired,
   onClickFunc: PropTypes.func.isRequired
 }
