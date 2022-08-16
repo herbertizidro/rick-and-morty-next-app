fdd1c3d51feeb3f86d4a33303e711f95cb775a5e
Herbert Izidro <herbertsouza80@yahoo.com>
2022-08-16 12:53:12 -0300

    Update SearchInput.js

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -5,7 +5,7 @@ export default function SearchInput(props) {
   const {search, onChangeFunc, onClickFunc} = props
   return (
 	<div id="input-search">
-		<input type="text" className="form-control rounded-0" id="search-input" placeholder="Search for a character" value={search} onChange={onChangeFunc}/>
+		<input type="text" className="form-control rounded-0" placeholder="Search for a character" value={search} onChange={onChangeFunc}/>
 		<button type="button" style={{backgroundColor: "#70aa46", borderColor: "#70aa46"}} className="btn btn-success rounded-0" onClick={onClickFunc}>Search</button>
 	</div>
   )
