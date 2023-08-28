9fcb89a96ff0c591d20adb9ca32f68bfbc71d687
Herbert Izidro <herbertsouza80@yahoo.com>
2023-08-27 23:31:26 -0300

    Update SearchInput.js

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types';
 import styles from '../styles/SearchInput.module.css'
 
 export default function SearchInput(props) {
-  const {search, onChangeFunc, onClickFunc, placeholder, buttonText} = props
+  const {value, onChange, onClick, placeholder, buttonText} = props;
   return (
 	<div id="input-search">
-		<input type="text" className="form-control rounded-0" placeholder={placeholder || ''} value={search} onChange={onChangeFunc}/>
-		<button type="button" style={{backgroundColor: "#70aa46", borderColor: "#70aa46"}} className="btn btn-success rounded-0" onClick={onClickFunc}>{buttonText}</button>
+		<input type="text" className="form-control rounded-0" placeholder={placeholder || ''} value={value} onChange={onChange}/>
+		<button type="button" style={{backgroundColor: "#70aa46", borderColor: "#70aa46"}} className="btn btn-success rounded-0" onClick={onClick}>{buttonText}</button>
 	</div>
   )
 };
 
 SearchInput.defaultProps = {
-  search: '',
+  value: '',
   placeholder: '',
 };
 
 SearchInput.propTypes = {
-  search: PropTypes.string,
+  value: PropTypes.string,
   placeholder: PropTypes.string,
   buttonText: PropTypes.string.isRequired,	
-  onChangeFunc: PropTypes.func.isRequired,
-  onClickFunc: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired
 };
