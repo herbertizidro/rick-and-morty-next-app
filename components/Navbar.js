c183789c1f2c3dd2f34303b937419141b48f3634
Herbert Izidro <herbertsouza80@yahoo.com>
2023-08-27 23:34:12 -0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
 		<div className={styles.logo}>
-			<h4><span>RickAndMorty</span> Challenge</h4>
+			<Link href="/"><h4><span>RickAndMorty</span> Challenge</h4></Link>
 		</div>
 		<ul className={styles.link_items}>
 			<li>
