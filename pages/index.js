4c2c7884f3b3bb0a7a3d5fb8195b0284332e16d7
Herbert Izidro <herbertsouza80@yahoo.com>
2024-03-19 01:47:17 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -82,6 +82,8 @@ const Home = props => {
 		if (results?.length) return results.map(item => <Card key={item.id} id={item.id} name={item.name} species={item.species} image={item.image} />);
 		return props.data.items?.map(item => <Card key={item.id} id={item.id} name={item.name} species={item.species} image={item.image} />)
 	}
+
+	const content = mountContent();
 	
 	return (
 		<>	
@@ -96,7 +98,7 @@ const Home = props => {
 		</div>
 		<div className={styles.generic_container}>{!results && <h6>all characters</h6>}</div>
 		
-	        {<div id="wrapper-cards" className={styles.generic_container}>{mountContent()}</div>}			
+	        <div id="wrapper-cards" className={styles.generic_container}>{content}</div>	
 	
 		{!results && (
 			<div id="paginate">
