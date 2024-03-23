69fb4c2f665be12f68666a80a141449199dc39b8
Herbert Izidro <herbertsouza80@yahoo.com>
2024-03-23 04:55:24 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -97,7 +97,7 @@ const Home = props => {
 		</div>
 		<div className={styles.generic_container}>{!results && <h6>all characters</h6>}</div>
 		
-	    <div id="wrapper-cards" className={styles.generic_container}>
+	    	<div id="wrapper-cards" className={styles.generic_container}>
 			{isLoading ? (
 				<div id="loader-full-screen"><div id="loader"><h5>Please, wait a moment ...</h5></div></div>
 			) : content }
